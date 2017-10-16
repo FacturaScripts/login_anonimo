1440332f185299bf35340dc90776ce72e7234046
Francesc Pineda Segarra <shawe.ewahs@gmail.com>
2017-10-16 18:23:27 +0200

    - Añadido configurador para activar/desactivar el chat de soporte

---
+++ view/js/chat_soporte.js
@@ -0,0 +1,9 @@
+// Start of Smartsupp Live Chat script
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'f691ee6282d7096f75db14a79b5541298283c6d9';
+window.smartsupp||(function(d) {
+    var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+    s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+    c.type='text/javascript';c.charset='utf-8';c.async=true;
+    c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
